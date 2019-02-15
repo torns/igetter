ad3499f6615e232e8ad3acb243e8a69dd25500d8
larendorr <larendorrx@gmail.com>
2019-02-15 23:20:23 +0800

    :tada: Init
    init webpack typescript config & package

---
+++ config/webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const pack = require("../package.json")
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+	context: resolve('src'),
+	entry: './index.ts',
+	devtool: "inline-source-map",
+	target: 'node',
+	output: {
+		filename: 'index.js',
+		path: resolve('dist'),
+		libraryTarget: 'umd'
+	},
+	externals: [...Object.keys(pack.dependencies)],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.ts','.tsx','.js','.json']
+	}
+}
